b6d988a80d7f7fc866d2567eeb22858a49f76ad5
code-vic <ehimigbaivictor26@gmail.com>
2022-11-17 13:17:13 +0100

    delete user feature partially completed

--- api/app.js
+++ api/app.js
@@ -1,15 +1,17 @@
-require('dotenv').config()
-require('./database') // load database
-const { urlencoded } = require('express'),
-    {cors} = require('./utils/cors'),
-    {development} = require("./config/development")
-    express = require('express'),
-    app = express()
+require("dotenv").config();
+require("./database"); // load database
+const userRouter = require("./routes/userRouter"); // importing user routes
+const { urlencoded } = require("express"),
+  { cors } = require("./utils/cors"),
+  { development } = require("./config/development");
+(express = require("express")), (app = express());
 
-
-
-app.use(express.json())
-    .use(cors) 
-    .use(express.urlencoded({ extended: false }))
-    .get('/', (req,res) => res.send('Connected'))
-    .listen(development.PORT, () =>  console.log(`App is running on port ${development.PORT}`)) 
+app
+  .use(express.json())
+  .use(cors)
+  .use(express.urlencoded({ extended: false }))
+  .use(userRouter)
+  .get("/", (req, res) => res.send("Connected"))
+  .listen(development.PORT, () =>
+    console.log(`App is running on port ${development.PORT}`)
+  );
