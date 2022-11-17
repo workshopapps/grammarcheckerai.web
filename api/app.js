7b5e6d995b0d2d9791f7cc568757ec4b6d81667c
code-vic <ehimigbaivictor26@gmail.com>
2022-11-17 20:28:39 +0100

    user_delete_feature almost done

--- api/app.js
+++ api/app.js
@@ -1,36 +1,16 @@
-<<<<<<< HEAD
-require("dotenv").config();
-require("./database"); // load database
-const userRouter = require("./routes/userRouter"); // importing user routes
-const { urlencoded } = require("express"),
-  { cors } = require("./utils/cors"),
-  { development } = require("./config/development");
-(express = require("express")), (app = express());
-
-app
-  .use(express.json())
-  .use(cors)
-  .use(express.urlencoded({ extended: false }))
-  .use(userRouter)
-  .get("/", (req, res) => res.send("Connected"))
-  .listen(development.PORT, () =>
-    console.log(`App is running on port ${development.PORT}`)
-  );
-=======
 require("./database/index.js"); //load databse
 
+const userRouter = require("./routes/userRouter"); // importing user routes
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
-app.use(express.json())
-   .use(cors);
-   
-app.use('/', (req, res)=>{
-    res.status(200).json({message: 'welcome'})
+app.use(express.json()).use(cors);
+
+app.use(userRouter);
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "welcome" });
 });
 
-
 exports.app = app;
->>>>>>> develop
