d16f92e1c9e4c8cbdc82f82b5083325b2a59e469
code-vic <ehimigbaivictor26@gmail.com>
2022-11-17 20:33:15 +0100

    second commit

--- api/app.js
+++ api/app.js
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+const userRouter = require("./routes/userRouter"); // importing user routes
+
+>>>>>>> Stashed changes
 require("./database/index.js"); //load databse
 
 const userRouter = require("./routes/userRouter"); // importing user routes
@@ -8,7 +13,10 @@ const app = express();
 
 app.use(express.json()).use(cors);
 
+<<<<<<< Updated upstream
 app.use(userRouter);
+=======
+>>>>>>> Stashed changes
 app.use("/", (req, res) => {
   res.status(200).json({ message: "welcome" });
 });
