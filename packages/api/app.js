7573ba1f12a44c6d00864079a8f1819470b31329
Joshua thomas <thomasjoshuaseun@gmail.com>
2022-11-18 15:33:56 +0100

    feat(BE-15-login): added login endpoint

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const login = require('./routes/loginRoute') //login
 const userRouter = require("./routes/userRouter"); // importing user routes
 
 require("./database/index.js"); //load databse
@@ -9,9 +10,9 @@ const app = express();
 app.use(express.json()).use(cors());
 
 app.delete("/user", userRouter);
-
 app.use("/", (req, res) => {
   res.status(200).json({ message: "welcome" });
 });
+app.use('/api/v1/login', login)
 
 exports.app = app;
