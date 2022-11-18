935f6789ab736ee9e2cc69a68daaf30572ae0f60
code-vic <ehimigbaivictor26@gmail.com>
2022-11-18 19:53:21 +0100

    delete_user_feature almost completed

--- packages/api/controller/userController.js
+++ packages/api/controller/userController.js
@@ -1,40 +1,47 @@
+const { userCollection } = require("../database/models/userSchema");
 // FOR DELETING A USER ACCOUNT
 //////////////////////////////////////////////////////////////////////////////////////////////////
 async function deleteUser(req, res) {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  // checking if all required field are provided
-  if (!email || !password) {
-    res.status(400);
-    res.json({ message: "please provide user email and password" });
-    return;
-  }
-  k8;
+    // checking if all required field are provided
+    if (!email || !password) {
+      res.status(400);
+      res.json({ message: "please provide user email and password" });
+      return;
+    }
 
-  // checking for user in our database using the email provided
-  let user;
+    // checking for user in our database using the email provided
+    const user = await userCollection.findOne({ email });
 
-  // if user does not exist
-  if (!user) {
-    res.status(404);
-    res.json({ message: "no user found with the email provided" });
-    return;
-  }
+    // if user does not exist
+    if (!user) {
+      res.status(404);
+      res.json({ message: "no user found with the email provided" });
+      return;
+    }
 
-  // verify that the user password is correct
-  let isCorrect;
-  // if password is not correct
-  if (!isCorrect) {
-    res.status(401);
-    res.json({ message: "you are not authorized to delete this account" });
-    return;
-  }
+    // verify that the user password is correct
+    let isCorrect;
+    // if password is not correct
+    if (!isCorrect) {
+      res.status(401);
+      res.json({ message: "you are not authorized to delete this account" });
+      return;
+    }
 
-  // if user exist and password is correct
-  if (user) {
-    // delete user from data base
-    res.status(200);
-    res.json({ message: "you have successfully deleted your account" });
+    // if user exist and password is correct
+    if (user && isCorrect) {
+      await userCollection.deleteOne({ email });
+      res.status(200);
+      res.json({ message: "you have successfully deleted your account" });
+    }
+  } catch (error) {
+    if (error) {
+      res.status(500);
+      res.json({ message: "Something went wrong" });
+    }
   }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////
