bde13ec0b7ac3bd26be64cc4b04dd0ee9bbf7200
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-11-18 21:04:35 +0100

    chore: Setup express-session

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+
+const { environment } = require("./config/environment");
 const userRouter = require("./routes/userRouter"); // importing user routes
 
 require("./database/index.js"); //load databse
@@ -7,6 +10,20 @@ require("./database/index.js"); //load databse
 const app = express();
 
 app.use(express.json()).use(cors());
+
+const sess = {
+  secret: environment.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {},
+};
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
 
 app.delete("/user", userRouter);
 
