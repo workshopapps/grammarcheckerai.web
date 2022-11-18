199ef7547c8a9f93f92cc2756892494c0bc2b4c0
johnbosco <johnboscoo@phreetech.com>
2022-11-18 12:58:42 +0100

    fix: accepted incoming changes on api/app.js

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-// const userRouter = require("./routes/userRouter"); // importing user routes
-
-// require("./database/index.js"); //load databse
-
-// const userRouter = require("./routes/userRouter"); // importing user routes
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-
-app.use(express.json()).use(cors);
-=======
-
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/userRouter"); // importing user routes
 
-require("./database/index.js"); //load databse 
+require("./database/index.js"); //load databse
 
 const app = express();
 
 app.use(express.json()).use(cors());
 
-app.delete('/user', userRouter)
->>>>>>> 91e521f3b1cb8296e19d5b467a84c63fc403d636
+app.delete("/user", userRouter);
 
 app.use("/", (req, res) => {
   res.status(200).json({ message: "welcome" });
