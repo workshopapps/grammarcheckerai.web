4e8335476fe53cb5b74e4627e2fb57421f8efa72
Njoli Patrick <36375033+ogmaro@users.noreply.github.com>
2022-11-18 09:57:00 +0100

    Api develop (#12)
    * updated package.lock
    
    Co-authored-by: johnbosco <johnboscoo@phreetech.com>

--- packages/api/config/environment.js
+++ packages/api/config/environment.js
@@ -5,5 +5,8 @@ config();
 exports.environment = {
     PORT: process.env.PORT,
     HOST: process.env.HOST,
-    DATABASE_URI: process.env.DATABASE_URI
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URI_PROD: process.env.DATABASE_URI_PROD,
+    DATABASE_URI_TEST: process.env.DATABASE_URI_TEST,
+    DATABASE_URI_DEVELOP: process.env.DATABASE_URI_DEVELOP
 };
