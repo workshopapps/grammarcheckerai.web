74a0aa7ea4e22d2678293f0d35869e83f7033f33
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-11-18 14:08:34 +0100

    chore: Add OPENAI_API_KEY env variable

--- packages/api/config/environment.js
+++ packages/api/config/environment.js
@@ -3,7 +3,8 @@ const { config } = require("dotenv");
 config();
 
 exports.environment = {
-    PORT: process.env.PORT,
-    HOST: process.env.HOST,
-    DATABASE_URI: process.env.DATABASE_URI
+  PORT: process.env.PORT,
+  HOST: process.env.HOST,
+  DATABASE_URI: process.env.DATABASE_URI,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 };
