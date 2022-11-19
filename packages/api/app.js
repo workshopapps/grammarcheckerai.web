e9b955350758a4bc7d89ce53f794e169e4c29c17
code-vic <ehimigbaivictor26@gmail.com>
2022-11-19 17:05:53 +0100

    resolving merge conflict

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const login = require('./routes/loginRoute') //login
-const logout = require('./routes/logoutRoute') //logout
+const login = require("./routes/loginRoute"); //login
+const logout = require("./routes/logoutRoute"); //logout
 const userRouter = require("./routes/userRouter"); // importing user routes
-const profile = require("./routes/userProfileRoute")// Get user profile
-
+const profile = require("./routes/userProfileRoute"); // Get user profile
 
 require("./database/index.js"); //load databse
 
@@ -12,16 +11,12 @@ const app = express();
 
 app.use(express.json()).use(cors());
 
-<<<<<<< HEAD
-app.use("/", userRouter);
-=======
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Grit Grammarly 🙌" });
 });
-app.use('/api/v1/login', login)
-app.use('/api/v1/logout', logout)
-app.use('/api/v1/user-profile/:id', profile)
+app.use("/api/v1/login", login);
+app.use("/api/v1/logout", logout);
+app.use("/api/v1/user-profile/:id", profile);
 app.delete("/user", userRouter);
->>>>>>> develop
 
 exports.app = app;
