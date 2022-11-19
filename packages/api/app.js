a139698b919b0885848df90f50bf1d42b39d45d1
code-vic <ehimigbaivictor26@gmail.com>
2022-11-19 17:34:49 +0100

    fixing merge conflicts

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const login = require("./routes/loginRoute"); //login
-const logout = require("./routes/logoutRoute"); //logout
-const deleteUser = require("./routes/deleteUserRouter"); //delete user
-const profile = require("./routes/userProfileRoute"); // Get user profile
+const express = require('express');
+const cors = require('cors');
+const login = require('./routes/loginRoute'); //login
+const logout = require('./routes/logoutRoute'); //logout
+const deleteUser = require('./routes/deleteUserRouter'); //delete user
+const profile = require('./routes/userProfileRoute'); // Get user profile
 
-require("./database/index.js"); //load databse
+require('./database/index.js'); //load databse
 
 const app = express();
 
 app.use(express.json()).use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Grit Grammarly 🙌" });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Grit Grammarly 🙌' });
 });
-app.use("/api/v1/login", login);
-app.use("/api/v1/logout", logout);
-app.use("/api/v1/user-profile/:id", profile);
-app.delete("/api/v1/delete-user", deleteUser);
+app.use('/api/v1/login', login);
+app.use('/api/v1/logout', logout);
+app.use('/api/v1/user-profile/:id', profile);
+app.delete('/user', userRoute);
 
 exports.app = app;
