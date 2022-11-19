6d9c54284e626b631b0ffdc899b7b3a76964dba1
akjesus <cutytongy@gmail.com>
2022-11-19 17:48:52 +0100

    feat(BAC-10): added more integration testing for endpoints

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan");
 const login = require('./routes/loginRoute') //login
 const logout = require('./routes/logoutRoute') //logout
 const userRouter = require("./routes/userRouter"); // importing user routes
+const profile = require('./routes/userProfileRoute')// importing profile routes
 
 require("./database/index.js"); //load databse
-const options = {
-  customCss: ".swagger-ui .topbar { display: none }",
-};
-
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, options)
-);
 app.use(express.json()).use(cors());
-
+app.use(morgan('tiny'));
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Grit Grammarly 🙌" });
+});
+app.use('/api/v1/login', login)
+app.use('/api/v1/logout', logout)
+app.use('/api/v1/user-profile/:id', profile)
 app.delete("/user", userRouter);
 
-app.use("/", (req, res) => {
-  res.status(200).json({ message: "welcome" });
-});
-
-exports.app = app;
+module.exports = app;
