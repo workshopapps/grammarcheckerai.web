ed84d1ce5b091dc5d9c87975ec89cf7d3daeaeea
Titiloye Temitope Michael <titimichael01@email.com>
2022-11-19 01:26:23 +0100

    Implemented Swagger-API-Testing Routes With Swagger-Spotlight-Studio

--- packages/api/app.js
+++ packages/api/app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/userRouter"); // importing user routes
 
+swaggerUi = require('swagger-ui-express')
+swaggerDocument = require('./Tests/test.json')
+
 require("./database/index.js"); //load databse
 
 const app = express();
 
-app.use(express.json()).use(cors());
+
+app.use(express.json()).use(cors())
+   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) // loaded swagger documentation
+
 
 app.delete("/user", userRouter);
 
