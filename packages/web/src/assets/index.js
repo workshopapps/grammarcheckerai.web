39a2323913d6b4e4fcca5b39bc8b779314062624
Ajayi Michael <ajayimichael150@gmail.com>
2022-11-19 18:06:23 +0100

    chore: added assets and imported them

---
+++ packages/web/src/assets/index.js
@@ -0,0 +1,9 @@
+import languageIcon from './icon/language-square.svg';
+import maximizeIcon from './icon/maximize-3.svg';
+import infinityIcon from './icon/unlimited.svg';
+import CountryIcon from './icon/british.svg';
+import EditIcon from './icon/edit-2.svg';
+import arrowRightIcon from './icon/arrow-right.svg';
+import searchIcon from './icon/search-normal.svg';
+
+export { languageIcon, maximizeIcon, infinityIcon, arrowRightIcon, EditIcon, CountryIcon, searchIcon };
