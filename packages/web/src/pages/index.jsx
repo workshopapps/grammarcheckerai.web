f56e1002ab5280419606927831dabcfb51886da2
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2022-11-19 21:13:04 +0100

    feat: added legal page

--- packages/web/src/pages/index.jsx
+++ packages/web/src/pages/index.jsx
@@ -1,4 +1,4 @@
 // Import and export on the same line from modules example
 // export { default as LandingPage } from "../modules/static/landing-page";
 
-export { default as LegalPage } from "./Legal/index";
+export { default as LegalPage } from './Legal/index';
