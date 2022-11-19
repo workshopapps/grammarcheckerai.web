215263a5fe76230157e04a3ae6d1cb92bb14f75d
divine <divineobi@gmail.com>
2022-11-19 23:37:35 +0100

    styles: change class names

--- packages/web/src/components/StatusReport/StatusReport.jsx
+++ packages/web/src/components/StatusReport/StatusReport.jsx
@@ -2,13 +2,12 @@ import style from './StatusReport.module.css';
 import PropTypes from 'prop-types';
 
 const StatusReport = ({ title, status }) => {
-  console.log(status.statusName);
   const { statusName, logo } = status;
   return (
-    <div className={style.systemReport}>
-      <p className={style.systemTitle}>{title}</p>
-      <p className={`${style.systemStatus} ${style[statusName]}`}>{statusName}</p>
-      <span className={style.systemReportLogo}>
+    <div className={style.statusReport}>
+      <p className={style.statusTitle}>{title}</p>
+      <p className={`${style.statusStatus} ${style[statusName]}`}>{statusName}</p>
+      <span className={style.statusReportLogo}>
         <img src={logo} alt="" className={style.statusLogo} />
       </span>
     </div>
