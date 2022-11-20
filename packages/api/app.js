3d1d5b5921b1d73a3b1b94bee19eeaa7ed2d60d4
eniola <opakunleeniolail2019@gmail.com>
2022-11-20 18:51:35 +0100

    fix(BAC-13): added the route to the endpoit

--- packages/api/app.js
+++ packages/api/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const login = require('./routes/loginRoute') //login
 const logout = require('./routes/logoutRoute') //logout
 const userRouter = require("./routes/userRouter"); // importing user routes
+const correction_history = require('./routes/correctionHistoryRoute')//correction history route
 
 
 require("./database/index.js"); //load databse
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 app.use('/api/v1/login', login)
 app.use('/api/v1/logout', logout)
+app.use('/api/v1/correctionhistory/:userId', correction_history )
 app.delete("/user", userRouter);
 
 exports.app = app;
