4981262a7812400d1531ed2ed2b365b58baa2a7e
akjesus <cutytongy@gmail.com>
2022-11-20 12:48:28 +0100

    feat:BAC-10 added endpoint for testing to frontend using axios

--- packages/api/app.js
+++ packages/api/app.js
@@ -22,6 +22,7 @@ const sess = {
   cookie: {},
 };
 
+
 if (app.get("env") === "production") {
   app.set("trust proxy", 1); // trust first proxy
   sess.cookie.secure = true; // serve secure cookies
--- packages/api/controller/testController.js
+++ packages/api/controller/testController.js
@@ -1,17 +1,62 @@
 const app = require("../app");
 const express = require("express");
-
+const axios = require("./axios");
 
 exports.home = async (req, res) => {
-    const status = {
-      home: { status: "ok" },
-      login: { status: "ok" },
-      signUp: { status: "ok" },
-      linkedIn: { status: "down" },
-      googleAuth: { status: "ok" },
-      logout: { status: "ok" },
-      sendAudio: { status: "ok" },
-    };
+  var status = [];
+  const home = await axios
+    .get("/")
+    .then((home) => {
+      if (home.status == 200) {
+        status.push({ home: { status: "ok" } });
+      }
+    })
+    .catch((err) => {
+      status.push({ home: { status: "down" } });
+    });
+  const loginPage = await axios
+    .get("/api/v1/auth/login")
+    .then((response) => {
+      if (response.status == 200) {
+        status.push({ loginPage: { status: "ok" } });
+      }
+    })
+    .catch((err) => {
+      status.push({ loginPage: { status: "down" } });
+    });
+
+    const sendAudio = await axios
+      .get("/api/v1/sendAudio")
+      .then((sendAudio) => {
+        if (sendAudio.status == 200) {
+          status.push({ sendAudio: { status: "ok" } });
+        }
+      })
+      .catch((err) => {
+        status.push({ sendAudio: { status: "down" } });
+      });
+
+      const logoutPage = await axios
+        .get("/api/v1/logout")
+        .then((response) => {
+          if (response.status == 200) {
+            status.push({ logoutPage: { status: "ok" } });
+          }
+        })
+        .catch((err) => {
+          status.push({ logoutPage: { status: "down" } });
+        });
+
+        const test = await axios
+          .get("/api/v1/test")
+          .then((response) => {
+            if (response.status == 200) {
+              status.push({ testPage: { status: "ok" } });
+            }
+          })
+          .catch((err) => {
+            status.push({ testPage: { status: "down" } });
+          });
+
   return res.status(200).send(status);
 };
-
