7667eb0314e4ab974d1c077d90ebd033da8bdd32
eniola <opakunleeniolail2019@gmail.com>
2022-11-20 22:36:22 +0100

    fix(BAC-13): made a change in the controller

--- packages/api/controller/correctionHistoryController.js
+++ packages/api/controller/correctionHistoryController.js
@@ -2,7 +2,7 @@ const {userResponsecollection} = require('../database/models/userResponseSchema'
 
 exports.history = async (req, res) =>{
     const {userId} = req.params
-    let history = await userResponsecollection.findOne({ userId })
+    let history = await userResponsecollection.find({ userId })
     if (!history){
         return res.status(401).json({ message: "No history available"})
     }
