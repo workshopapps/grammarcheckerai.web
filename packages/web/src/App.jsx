68e412dd78e1224b37c2cf65a490921a3d820ebc
johnbosco <johnboscoo@phreetech.com>
2022-11-20 10:04:23 +0100

    fixed: added legal route

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -11,7 +11,7 @@ import ResetLink from './modules/auth/reset-password/reset';
 import TermsOfUse from './modules/static/terms_of_use';
 import DashboardLayout from './components/DashboardLayout';
 import NewsletterPage from './modules/static/newsletter/NewsletterPage';
-import { HomePage, History, Correction, ConversationPage, LandingPage } from './pages';
+import { HomePage, History, Correction, ConversationPage, LandingPage, LegalPage } from './pages';
 
 // All routes/pages must be import from ./pages folder
 const router = createBrowserRouter(
@@ -27,7 +27,7 @@ const router = createBrowserRouter(
       <Route path="/faq" element={<h1>FAQ Page</h1>} />
       <Route path="/blog" element={<h1>Blog Page</h1>} />
       <Route path="contact" element={<h1>Contact Page</h1>} />
-      <Route path="/newsletter" element={<NewsletterPage/>} />
+      <Route path="/newsletter" element={<NewsletterPage />} />
       <Route path="/culture-career" element={<h1>Culture Page</h1>} />
       <Route path="/terms-of-use" element={<TermsOfUse />} />
       <Route path="/api-status" element={<h1>Api status Page</h1>} />
@@ -92,7 +92,6 @@ function App() {
   }, []);
   console.log(demoData);
   return <RouterProvider router={router} />;
-  
 }
 
 export default App;
