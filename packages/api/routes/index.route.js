fd83e46af3400de4bf25f77d15bbf087dedb374e
Njoli Patrick <ogmaro@gmail.com>
2022-11-20 15:42:56 +0100

    minor changes

--- packages/api/routes/index.route.js
+++ packages/api/routes/index.route.js
@@ -3,7 +3,7 @@ const { userHandler } = require("../routes/user.routes.js");
 const { auth } = require("../routes/auth.routes");
 const conversationRouter = require("./conversationRouter");
 
-const verify = require("../middlewares/authMiddleware");
+const verify = require("../middlewares/auth.middleware");
 
 routeHandler.use("/auth", auth);
 routeHandler.use("/user", verify, userHandler);
