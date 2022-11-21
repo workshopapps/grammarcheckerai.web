fb3d3a948f958989c92a5736cde83cc0d5cf1f13
Njoli Patrick <ogmaro@gmail.com>
2022-11-21 15:03:18 +0100

    chore: removed api on routes

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const session = require('express-session')
+const session = require("express-session");
 
 const { environment } = require("./config/environment");
-const testRoute = require('./routes/testRoutes')
-require('express-async-errors')
-require('./database/index')
-const passport = require('passport');
-require('./services/linkedinStrategy') 
-const {routeHandler} = require('./routes/index.route'),
-    swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./Tests/test.json')
-
+const testRoute = require("./routes/testRoutes");
+require("express-async-errors");
+require("./database/index");
+const passport = require("passport");
+require("./services/linkedinStrategy");
+const { routeHandler } = require("./routes/index.route"),
+  swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./Tests/test.json");
 
 //Passport Initialized
 app.use(passport.initialize());
 
-app.use(cors({
-  origin: '*',
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
-app.use(express.json()); 
+app.use(express.json());
 
 const sess = {
   secret: environment.SESSION_SECRET,
@@ -31,22 +32,18 @@ const sess = {
   cookie: {},
 };
 
-
 if (app.get("env") === "production") {
   app.set("trust proxy", 1); // trust first proxy
   sess.cookie.secure = true; // serve secure cookies
 }
 
-app.use(session(sess))
-   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app
+  .use(session(sess))
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
-app.use("/api/v1/testRoute", (req, res) => {
-  res.status(200).json({ message: "working" });
-});
-app.use("/test", test);
-app.use("/api/v1", routeHandler);
-app.get("/", (req, res) => {
+app.use("/test", testRoute);
+app.use("/v1", routeHandler);
+app.get("*", (req, res) => {
   res.status(200).json({ message: "Welcome to Grit Grammarly 🙌" });
 });
 module.exports = app;
