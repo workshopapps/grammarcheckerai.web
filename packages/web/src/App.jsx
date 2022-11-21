feca6c1c1a42d82cde643bcdb4191701cd3dc604
johnbosco <johnboscoo@phreetech.com>
2022-11-21 03:09:20 +0100

    chore: cleaned up app.js

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import './App.css';
-import React, { useEffect } from 'react';
 import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements, Outlet } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import { useLocalStorage } from './hooks/useLocalStorage';
@@ -34,6 +34,7 @@ import Careers from './pages/career/Career';
 import Roles from './pages/career/Roles';
 import Application from './pages/career/Application';
 import ApiStatus from './pages/api-status/api-status';
+import LandingLayout from './components/LandingLayout.jsx';
 
 // All routes/pages must be import from ./pages folder
 const router = createBrowserRouter(
@@ -41,13 +42,13 @@ const router = createBrowserRouter(
     <>
       <Route path="/" element={<ConversationPage />} />
       <Route path="/history" element={<h2>History</h2>} />
-      <Route path="/gritty-grammar" element={<LandingPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/history" element={<History />} />
+      <Route element={<LandingLayout />}>
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/faq" element={<FaqMain />} />
+      </Route>
       <Route path="/settings" element={<Settings />} />
       <Route path="/correction" element={<Correction />} />
       <Route path="/about" element={<h1>About Page</h1>} />
-      <Route path="/faq" element={<FaqMain />} />
       <Route path="/blog" element={<h1>Blog Page</h1>} />
       <Route path="contact" element={<h1>Contact Page</h1>} />
       <Route path="/newsletter" element={<NewsletterPage />} />
@@ -125,11 +126,6 @@ const router = createBrowserRouter(
 );
 
 function App() {
-  const [demoData, setDemoData] = useLocalStorage('demoData', '');
-  useEffect(() => {
-    setDemoData('demo');
-  }, []);
-  console.log(demoData);
   return <RouterProvider router={router} />;
 }
 
--- packages/web/src/components/Navbar/index.jsx
+++ packages/web/src/components/Navbar/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Navbar = () => {
   return (
-    <header className="flex justify-between py-4 sm:px-10 items-center z-10 fixed top-0 w-full bg-white px-3 shadow-sm">
+    <header className="flex justify-between py-4 sm:px-10 items-center z-10 fixed top-0 w-full bg-[#5D387F] text-white px-3">
       <div>
-        <img src="images/grit.svg" alt="gritty" />
+        <img src="images/grit.svg" alt="gritty" className="salu\" />
       </div>
       <div className="sm:flex space-x-12 hidden ">
         {[
-          { title: 'Home', to: '/' },
+          { title: 'Converse', to: '/' },
           { title: 'About', to: '/about' },
           { title: 'FAQ', to: '/faq' },
           { title: 'Blog', to: '/blog' },
           { title: 'Contact', to: '/contact' },
         ].map((item) => (
-          <NavLink className="text-slate-600 z-30" to={item.to} key={item.title}>
+          <NavLink to={item.to} key={item.title}>
             {item.title}
           </NavLink>
         ))}
@@ -25,4 +25,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default Navbar;
