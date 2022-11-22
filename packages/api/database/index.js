64d26fa80290e6b5825733b6e67129ce5959e26e
Njoli Patrick <36375033+ogmaro@users.noreply.github.com>
2022-11-22 10:14:14 +0100

    chore: updated db connection (#138)

--- packages/api/database/index.js
+++ packages/api/database/index.js
@@ -1,41 +1,47 @@
-const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb');
-const { environment } = require('../config/environment.js');
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+const { environment } = require("../config/environment.js");
 
-const { NODE_ENV, DATABASE_URI_DEVELOP, DATABASE_URI_TEST, ME_CONFIG_MONGODB_URL } =
-  environment;
+const {
+  NODE_ENV,
+  DATABASE_URI_DEVELOP,
+  DATABASE_URI_TEST,
+  ME_CONFIG_MONGODB_URL,
+  ME_CONFIG_MONGODB_ADMINUSERNAME,
+  ME_CONFIG_MONGODB_ADMINPASSWORD,
+} = environment;
 
-  const db = (URi) => {
-    mongoose
-      .connect(URi, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => console.log(`MongoDB connected Successful 🚀`))
-      .catch((error) => console.log(error.message));
-    mongoose.Promise = global.Promise;
-  
-    mongoose.connection.on("connected", () => {
-      console.log("Mongoose connected to db...");
+const db = (URi) => {
+  mongoose
+    .connect(URi, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`MongoDB connected Successful 🚀`))
+    .catch((error) => console.log(error.message));
+  mongoose.Promise = global.Promise;
+
+  mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected to db...");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log(err.message);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose connection is disconnected...");
+  });
+
+  process.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+      console.log(
+        "Mongoose connection is disconnected due to app termination..."
+      );
+      process.exit(0);
     });
-  
-    mongoose.connection.on("error", (err) => {
-      console.log(err.message);
-    });
-  
-    mongoose.connection.on("disconnected", () => {
-      console.log("Mongoose connection is disconnected...");
-    });
-  
-    process.on("SIGINT", () => {
-      mongoose.connection.close(() => {
-        console.log(
-          "Mongoose connection is disconnected due to app termination..."
-        );
-        process.exit(0);
-      });
-    });
-  }; 
+  });
+};
 
 if (NODE_ENV === "development") {
   console.log(`DB running in ${NODE_ENV} mode`);
@@ -47,7 +53,5 @@ if (NODE_ENV === "test") {
 }
 if (NODE_ENV === "production") {
   console.log(`DB running in ${NODE_ENV} mode`);
-
-  module.exports = db(`mongodb://grittygrammar://${encodeURIComponent("grittypass")}@mongo:27017/`);
-
+  module.exports = db(`mongodb://mongo:27017/grittygrammar`);
 }
