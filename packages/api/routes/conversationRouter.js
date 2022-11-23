9ba2b0112bdc170b8a66a0736d861720bf0c9401
Titiloye Temitope Michael <titimichael01@email.com>
2022-11-23 10:10:16 +0100

    Created S3 Bucket on AWS and getting File URL from the saved file

--- packages/api/routes/conversationRouter.js
+++ packages/api/routes/conversationRouter.js
@@ -5,8 +5,20 @@ const endConversation = require('../controller/endConversationController');
 const startConversation = require('../controller/startConversationController');
 const uploadFile = require('../middlewares/audio.middleware.js');
 
+const create = require ("../middlewares/s3.js");
+const uploadAudio = require('../middlewares/s3Bucket.js');
+const saveAudio = require('../controller/uploadAudioController.js');
+
+
+conversationRouter.post('/createBucket', create);
+conversationRouter.post('/uploadAudio', uploadAudio.single("file"), saveAudio);
+
+
+
 conversationRouter.get('/start', startConversation);
 conversationRouter.get('/end', endConversation);
 conversationRouter.post('/sendAudio', uploadFile, getBotResponse);
+
+
 
 module.exports = conversationRouter;
