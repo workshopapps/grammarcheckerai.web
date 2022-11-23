4f0dfd1bbac60bfd1dac7c08469675eb1aa70587
Gloria <ubahgloria16@gmail.com>
2022-11-23 11:10:05 +0100

    Styles: feat(FRO-12-BLOG) added additional styles

--- packages/web/src/components/Blogcomponents/Header.jsx
+++ packages/web/src/components/Blogcomponents/Header.jsx
@@ -4,11 +4,11 @@ import Headerimg from '../../assets/blogimg/Rectangle 4.svg';
 const Header = () => {
   return (
     <header>
-      <div className="p-4 mb-5 relative h-64 md:p-0 xl:mb-8">
+      <div className="mb-5 relative h-64 md:p-0 xl:mb-8">
         <img src={Headerimg} alt="" className="w-full h-64 object-cover" />
 
         <p className="text-white absolute mx-auto text-center inset-0 w-48 font-bold py-3 leading-10 my-5 text-xl xl:text-4xl xl:w-2/5 xl:my-8">
-          The World`s Most <span className="text-span ">Dangerous Technology</span> Ever Made.
+          The World&apos;s Most <span className="text-span ">Dangerous Technology</span> Ever Made.
         </p>
       </div>
     </header>
--- packages/web/src/components/Blogcomponents/Hero.jsx
+++ packages/web/src/components/Blogcomponents/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
      return(
           <section className="mb-10">
                <div className="md:flex justify-between items-center flex-row-reverse md:max-w-3xl md:mx-auto xl:max-w-5xl">
-                    <div className="p-4 mb-4 md:w-80 md:h-full md:mb-0 md:p-0 xl:w-3/4">
+                    <div className="mb-4 md:w-80 md:h-full md:mb-0 md:p-0 xl:w-3/4">
                          <img src ={Heroimg} alt="Heroimg" className="md:w-full md:h-full" />
                     </div>
 
@@ -16,7 +16,7 @@ const Hero = () => {
 
                          <p className="font-normal text-sm mb-3 md:w-80">When we needed to punch code into a command line just to load a program, computers were far less user-friendly. But the mouse and graphical interfaces made things much easier, and computers blossomed from niche products into the mainstream.....</p>
                          
-                         <Link to="/ai" className="text-header text-sm font-bold mb-3 text-left">Read more</Link>
+                         <Link to="/ai" className="text-header text-sm font-bold mb-3 text-left hover:text-[#5d387f]">Read more</Link>
                     </div>  
                </div>
                
