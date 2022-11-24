1af3f59bdff75d6af7b401bf574107b892567b6b
eleezs <wilduche@gmail.com>
2022-11-24 14:47:12 +0100

    ft/reset password process

--- packages/api/routes/auth.routes.js
+++ packages/api/routes/auth.routes.js
@@ -3,6 +3,8 @@ const auth = Router();
 const {
   validate,
   registerValidationRules,
+	reset_password,
+	request_reset_password,
 } = require("../utilities/validation/auth.validation");
 const {
   registerUser, googleAuthUserSignUp
@@ -12,12 +14,14 @@ const { linkedin } = require("./linkedin-auth");
 const {login} = require('../controller/loginController')
 const {logout} = require('../controller/logoutcontroller') 
 const facebookAuthRoutes = require('./facebookAuth');
+const { requestForgotPassword, resetPassword } = require("../controller/forgotPasswordController");
  
 
 auth.post("/signup", registerValidationRules(), validate, registerUser);
 auth.get("/google", googleAuthURL);
 auth.post("/google", googleAuthUserSignUp);
-
+auth.post("/request-password-reset",validate(request_reset_password), requestForgotPassword)
+auth.post("/password-reset",validate(reset_password), resetPassword)
 
 auth.post("/login", login);
 auth.post("/logout", logout);
