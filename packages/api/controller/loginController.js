95e931c0af8078871ab74cdb3a7f8c84f1d4a8d2
Otong Akan <cutytongy@gmail.com>
2022-11-24 22:41:41 +0100

    Testing new branch (#189)
    * chore: fixed merge confict
    
    * chore: fixed package.json
    
    * tested all endpoint working ok
    
    * bug: fixed validation error
    
    * bug: restored login code
    
    * New Backend tested ok
    
    Co-authored-by: Njoli Patrick <ogmaro@gmail.com>

--- packages/api/controller/loginController.js
+++ packages/api/controller/loginController.js
@@ -1,39 +1,36 @@
-const {
-  userCollection,
-  authValidatorSchema,
-} = require('../database/models/userSchema');
-const { findOne } = require('../repository/user.repository');
-const { response } = require('../utilities/response');
+const { userCollection, authValidatorSchema } = require('../database/models/userSchema')
+const {  findOne } = require("../repository/user.repository");
+const {response} = require("../utilities/response")
 
-async function login(req, res) {
-  // retrieve the email and password
-  const { email, password } = req.body;
+ async function login (req, res) {
+    // retrieve the email and password 
+    const { email, password } = req.body;
 
-  const { error } = authValidatorSchema.validate({ email, password });
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = authValidatorSchema.validate({ email, password });
+    if (error) return res.status(400).send(error.details[0].message);
 
-  // check if the email and password exists
-  if (!email || !password) {
-    return res.status(404).json({ msg: 'Please provide email or password' });
-  }
+    // check if the email and password exists
+    if (!email || !password) {
+        return res.status(404).json({ msg: 'Please provide email or password' })
+    }
+    
+    let user = await userCollection.findOne({email});
+    
+    if (!user) {
+        return res.status(401).json({ msg: 'Invalid email or password' })
+    }
+    // comparing password
+    const validPassword = user.comparePassword(password)
 
-  let user = await userCollection.findOne({ email });
-
-  if (!user) {
-    return res.status(401).json({ msg: 'Invalid email or password' });
-  }
-  // comparing password
-  const validPassword = user.comparePassword(password);
-
-  if (!validPassword) {
-    return res.status(401).json({ msg: 'Invalid email or password' });
-  }
-  return res.status(200).json(
-    response({
-      success: true,
-      message: 'User login successfully',
-      data: user,
-    })
-  );
+    if (!validPassword) {
+        return res.status(401).json({ msg: 'Invalid email or password' })
+    }
+    return res.status(200).json(
+        response({
+          success: true,
+          message: "User login successfully",
+          data: user,
+        })
+      );
 }
-module.exports = { login };
+module.exports = {login}
