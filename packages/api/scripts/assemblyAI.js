b77bb9905f3973cf4c8d00a1723d18eb2bc35061
Mohammed Akanji <akanjimm@gmail.com>
2022-11-24 09:52:35 +0100

    chore: update .env and assemblyAI.js file with time

--- packages/api/scripts/assemblyAI.js
+++ packages/api/scripts/assemblyAI.js
@@ -75,6 +75,7 @@ exports.uploadFileUrlToInitiateTranscription = async function (audioUrl) {
 
 // function to get transcript and metadata from Assembly AI using the unique id provided as argument
 exports.getTranscriptionFromAssembly = async function (id) {
+    let transcriptStartTime = new Date();
     console.log("-------------------- Transcript download process started ----------------------")
     const url = `https://api.assemblyai.com/v2/transcript/${id}`;
 
@@ -98,10 +99,12 @@ exports.getTranscriptionFromAssembly = async function (id) {
                 await sleep(1000);
             } else if (count === 0) {
                 endTime = new Date();
+                console.log("transcript download time - success: ", (transcriptStartTime - endTime)/1000);
                 console.log("Transcription processing time - failed: ", (endTime - startTime)/1000);
                 throw new Error("Time out! Please try again.");
             } else {
                 endTime = new Date();
+                console.log("transcript download time - success: ", (transcriptStartTime - endTime)/1000);
                 console.log("Transcription processing time - success: ", (endTime - startTime)/1000);
                 return jsonResponse.text;
             }
