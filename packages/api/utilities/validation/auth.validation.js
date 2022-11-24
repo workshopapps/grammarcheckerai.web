4e9f12076ee6d2680d83278e803f6f9f83a2cdda
Njoli Patrick <ogmaro@gmail.com>
2022-11-24 22:07:24 +0100

    bug: fixed validation error

--- packages/api/utilities/validation/auth.validation.js
+++ packages/api/utilities/validation/auth.validation.js
@@ -11,16 +11,14 @@ const validate = (req, res, next) => {
   errors.array().map((err) => resultErrors.push({ [err.param]: err.msg }));
 
   const errorObject = Object.assign({}, ...resultErrors);
-  return res
-    .status(422)
-    .json(
-      response({
-        success: false,
-        message: "Action unsuccessful",
-        error: "Validation failed",
-        data: errorObject,
-      })
-    );
+  return res.status(422).json(
+    response({
+      success: false,
+      message: "Action unsuccessful",
+      error: "Validation failed",
+      data: errorObject,
+    })
+  );
 };
 /**
  * It checks if the payload contains only the fields that are allowed
@@ -28,12 +26,12 @@ const validate = (req, res, next) => {
  * @param fields - an array of allowed fields
  * @returns true
  */
- const checkAllowedFields = (payload, fields) => {
+const checkAllowedFields = (payload, fields) => {
   payload = Array.isArray(payload) ? payload : [payload];
 
   payload.forEach((item) => {
-    const allowed = Object.keys(item).every(field => fields.includes(field));
-    fields = typeof fields === 'string' ? fields : fields.join(', ');
+    const allowed = Object.keys(item).every((field) => fields.includes(field));
+    fields = typeof fields === "string" ? fields : fields.join(", ");
 
     if (!allowed) {
       throw new Error(`Wrong fields passed. Allowed fields: ${fields}`);
@@ -41,7 +39,7 @@ const validate = (req, res, next) => {
   });
 
   return true;
-}
+};
 
 const registerValidationRules = () => {
   return [
@@ -123,55 +121,61 @@ const emailValidationRules = () => {
   ];
 };
 
+const reset_password = () => {
+  return [
+    body("new_password")
+      .exists()
+      .withMessage("New password is required")
+      .trim()
+      .notEmpty()
+      .withMessage("Password can not be empty")
+      .isLength({ min: 6, max: 16 })
+      .withMessage("Password must be between 6 and 16 characters"),
+    body("confirm_password")
+      .exists()
+      .withMessage("Confirm password is required")
+      .trim()
+      .notEmpty()
+      .withMessage("Password can not be empty")
+      .isLength({ min: 6, max: 16 })
+      .withMessage("Password must be between 6 and 16 characters"),
+    body("user_token")
+      .exists()
+      .withMessage("user token is required")
+      .isString()
+      .withMessage("User token must be a string")
+      .notEmpty()
+      .withMessage("User token can not be empty"),
+    body().custom((body) =>
+      checkAllowedFields(body, [
+        "new_password",
+        "confirm_passowrd",
+        "user_token",
+      ])
+    ),
+  ];
+};
 
-const reset_password = [
-	body("new_password")
-		.exists()
-		.withMessage('New password is required')
-		.trim()
-		.notEmpty()
-		.withMessage("Password can not be empty")
-		.isLength({ min: 6, max: 16 })
-		.withMessage("Password must be between 6 and 16 characters"),
-  body("confirm_password")
-		.exists()
-		.withMessage('Confirm password is required')
-		.trim()
-		.notEmpty()
-		.withMessage("Password can not be empty")
-		.isLength({ min: 6, max: 16 })
-		.withMessage("Password must be between 6 and 16 characters"),
-	body("user_token")
-		.exists()
-		.withMessage('user token is required')
-		.isString()
-		.withMessage('User token must be a string')
-		.notEmpty()
-		.withMessage("User token can not be empty"),
-	body()
-		.custom(body => checkAllowedFields(body, ['new_password','confirm_passowrd', 'user_token']))
-]
-
-const request_reset_password = [
-	body("email")
-		.exists()
-		.withMessage('Email is required')
-		.trim()
-		.notEmpty()
-		.withMessage("Email can not be empty")
-		.trim()
-		.isEmail()
-		.normalizeEmail()
-		.withMessage("please enter a valid email"),
-	body()
-		.custom(body => checkAllowedFields(body, 'email'))
-]
-
+const request_reset_password = ()=>{
+  return [
+    body("email")
+      .exists()
+      .withMessage("Email is required")
+      .trim()
+      .notEmpty()
+      .withMessage("Email can not be empty")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("please enter a valid email"),
+    body().custom((body) => checkAllowedFields(body, "email")),
+  ]
+};
 
 module.exports = {
   validate,
   registerValidationRules,
   loginValidationRules,
-	request_reset_password,
-	reset_password
+  request_reset_password,
+  reset_password,
 };
