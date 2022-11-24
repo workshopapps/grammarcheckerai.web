b8ada48e52cd66dd3ebd8ccb3cfa040d8fe02391
akjesus <cutytongy@gmail.com>
2022-11-24 09:28:34 +0100

    Added Session to Random Question Feature

--- packages/api/controller/quizController.js
+++ packages/api/controller/quizController.js
@@ -1,22 +1,52 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 
-exports.getQuiz= async (req, res) => {
-  var questionList = [];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "JHGF>,./?;;LJ8#$?,KL:>>>,,KJJJDHE",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//pass like this
+
+exports.getQuiz = async (req, res) => {
+  let sess = req.session;
+  sess.user = req.user;
+  console.log(req);
   await axios
     .get("https://the-trivia-api.com/api/questions?limit=1")
-    .then((question) => {
-     questionList.push(question.data[0]);
+    .then((quiz) => {
+      var question = quiz.data[0];
+      sess.questionId = question.id;
+      sess.question = question.question;
+      sess.options = question.incorrectAnswers;
+      sess.answer = question.correctAnswer;
+      question.incorrectAnswers.push(question.correctAnswer);
+      console.log(sess);
+      res.status(200).send(question);
     })
     .catch((err) => {
       console.log(err);
-      res.send("There is a Problem, please try again");
+      res.status(400).send("There is a Problem, please try again");
     });
-    res.status(200).send(questionList)
 };
 
 exports.sendAnswer = async (req, res) => {
-  var answer = [{"answer": "correct"}];
-  
-  res.status(200).send(answer);
+  const answer = req.body.answer;
+  var sess = req.session;
+  if (!sess.question)
+    return res.status(400).send("No question being responded to");
+  var message = "";
+  if (answer == sess.answer) {
+    message = { message: "Correct answer, weldone!" };
+  } else message = { message: "Incorrect answer, please try again!" };
+  res.status(200).send(message);
+  req.session.destroy();
 };
-
