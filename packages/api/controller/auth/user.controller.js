c489c5ab190ba6faf74c468ba25e58e317fc636f
akjesus <cutytongy@gmail.com>
2022-11-30 20:16:58 +0100

    FEAT: Implemented Emails on Signup, Forgot and Reset Password

--- packages/api/controller/auth/user.controller.js
+++ packages/api/controller/auth/user.controller.js
@@ -1,11 +1,11 @@
 const { response } = require("../../utilities/response");
 const { getTokens } = require("./google.user.controller");
-const { register } = require("../../repository/user.repository"); 
+const { register } = require("../../repository/user.repository");
 const { userCollection } = require("../../database/models/userSchema");
-const { slugify } = require("../../utilities/compare"); 
+const { slugify } = require("../../utilities/compare");
 const emailService = require("../../services/email.service");
 const { environment } = require("../../config/environment");
-const { SIGNUP_TEMPLATE_ID } = environment;
+const { SIGNUP_TEMPLATE_ID, BASE_URL } = environment;
 
 async function registerUser(req, res) {
   let {
@@ -37,13 +37,13 @@ async function registerUser(req, res) {
 
   const data = { email, firstName, lastName, username, password, language };
 
-  await emailService({
-    to: email, 
+  emailService({
+    to: email,
     subject: "Welcome to Speak Better",
     templateId: SIGNUP_TEMPLATE_ID,
     data: {
       name: firstName,
-      action_url: "/signin",
+      url: `${BASE_URL}/signin`,
     },
   });
 
