9b5820e96c2aa7cee1048640076b02cebb354f91
foluke <folukemi@fincra.com>
2022-12-01 12:13:10 +0100

    improvement on careers and cultures page

--- packages/web/src/components/Careers/teamFeedback.jsx
+++ packages/web/src/components/Careers/teamFeedback.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const TeamFeedback = ({ img, feedback, name, role }) => {
   return (
     <div className="flex w-11/12 items-center mx-auto gap-x-8 max-[480px]:flex-col max-[480px]:w-full max-[480px]:space-y-3">
-      <div className="max-[480px]:w-32 max-[480px]:h-32 w-56 rounded-full overflow-hidden">
+      <div className="max-[480px]:w-32 max-[480px]:h-32 w-48 rounded-full overflow-hidden">
         <img src={img} alt={name} className="object-cover w-full h-full" />
       </div>
       <div className="max-[480px]:text-center w-2/3 space-y-2">
