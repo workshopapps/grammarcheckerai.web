f9047f3b03877eb30784a5242851a5454b14e9f1
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2022-12-02 11:51:13 +0100

    fix: fixed API implementation to display the status properly

--- packages/web/src/pages/api-status/Report/ReportDetail.jsx
+++ packages/web/src/pages/api-status/Report/ReportDetail.jsx
@@ -5,84 +5,35 @@ import StatusReport from '../../../components/StatusReport/StatusReport';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const report = [
-  {
-    title: 'Testing/swagger GUI',
-    status: ValidateStatus(SUCCESS),
-  },
-  {
-    title: 'Sign-in (Apple)',
-    status: ValidateStatus(SUCCESS),
-  },
-  {
-    title: 'Sign-in (Facebook)',
-    status: ValidateStatus(SUCCESS),
-  },
-  {
-    title: 'Audio to whisper script',
-    status: ValidateStatus(FAILED),
-  },
-  {
-    title: 'Audio (/sendaudio)',
-    status: ValidateStatus(SUCCESS),
-  },
-  
-  {
-    title: 'Audio to whisper script',
-    status: ValidateStatus(SUCCESS),
-  },
-  {
-    title: 'Reset & forgotten password',
-    status: ValidateStatus(WARNING),
-  },
-  {
-    title: "Delete User",
-    status: ValidateStatus(SUCCESS)
-  },
-  {
-    title: 'Logout',
-    status: ValidateStatus(FAILED),
-  },
-  {
-    title: 'Update User',
-    status: ValidateStatus(SUCCESS),
-  },
-
-];
-
 const Detail = () => {
   const [status, setStatus] = useState([]);
-  // const apiTest = async () => {
-  //   const data = await axios.get('https://grittygrammar.hng.tech/api/v1/test')
-  //   console.log(data);
-  //   data.data.map((item, index) => {
-    
-  //   let statusImg = ''
-  //     if(item[Object.keys(item)].status === 'ok'){
-  //       statusImg = "SUCCESS"
-  //     }else if( item[Object.keys(item)].status === 'down'){
-  //       statusImg = "FAILED"
-  //     }else{
-  //       statusImg = "WARNING"
-  //     }
-    
-  //     setStatus(status => [...status, {title: Object.keys(item), status: ValidateStatus(statusImg)}] );
+  const apiTest = async () => {
+    const data = await axios.get('http://api.speakbetter.hng.tech/v1/test')
+    console.log(data);
+    data.data.map((item, index) => {
 
-  //   }
-  // )
-  // } 
-  // useEffect(() => {
-  //   apiTest() 
-  // }, [])
+      let statusImg = ''
+      if (item[Object.keys(item)].status === 'ok') {
+        statusImg = "SUCCESS"
+      } else if (item[Object.keys(item)].status === 'down') {
+        statusImg = "FAILED"
+      } else {
+        statusImg = "WARNING"
+      }
+
+      setStatus(status => [...status, { title: Object.keys(item), status: ValidateStatus(statusImg) }]);
+    }
+    )
+  }
+  useEffect(() => {
+    apiTest()
+  }, [])
 
   return (
     <section className={style.detail}>
       <h1 className={style.detailHeader}>All System Operation report</h1>
       <div className={style.mainDetail}>
-        {/* {status?.map((el, i) => (
-          <StatusReport title={el.title} status={el.status} key={i} />
-        ))} */}
-        {report?.map((el, i) => (
+        {status?.map((el, i) => (
           <StatusReport title={el.title} status={el.status} key={i} />
         ))}
       </div>
