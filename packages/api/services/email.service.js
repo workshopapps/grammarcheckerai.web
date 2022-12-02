303c9432093648c05d76f021345261b52f0254d3
Njoli Patrick <36375033+ogmaro@users.noreply.github.com>
2022-12-03 00:27:45 +0100

    Bac2 38 fixed reset password feature  (#324)

--- packages/api/services/email.service.js
+++ packages/api/services/email.service.js
@@ -1,22 +1,18 @@
 const sendgrid = require("@sendgrid/mail");
 const { environment } = require("../config/environment.js");
-const { SENDGRID_API_KEY, SENDGRID_EMAIL_FROM } = environment; 
+const { SENDGRID_API_KEY, SENDGRID_EMAIL_FROM } = environment;
 sendgrid.setApiKey(SENDGRID_API_KEY);
 
-const emailService = async ({
-  to, 
-  templateId,
-  dynamic_template_data,
-}) => {
+const emailService = async ({ to, templateId, dynamic_template_data }) => {
   return new Promise(async (resolve, reject) => {
     try {
       await sendgrid.send({
         from: `Speak Better <${SENDGRID_EMAIL_FROM}>`,
-        to, 
+        to,
         templateId,
         dynamic_template_data,
       });
-
+      console.log({ to, templateId, dynamic_template_data })
       resolve(true);
     } catch (err) {  
       console.log(err)
