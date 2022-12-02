95fa7d0c205c38c98fbd549603e801a3d47aa176
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-12-02 10:14:37 +0100

    implemented image upload to s3 bucket using multer-s3

---
+++ packages/api/middlewares/image.middleware.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const path = require('path');
+const { environment } = require('../config/environment');
+const { ACCESSKEYID, S3SECRETEKEY, GRITTYBUCKETNAME } = environment;
+
+const s3 = new aws.S3({
+  accessKeyId: ACCESSKEYID,
+  secretAccessKey: S3SECRETEKEY,
+  region: 'us-east-1',
+  Bucket: GRITTYBUCKETNAME,
+});
+
+const profileImgUpload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: GRITTYBUCKETNAME,
+    key: function (req, file, cb) {
+      cb(
+        null,
+        path.basename(file.originalname, path.extname(file.originalname)) +
+          '-' +
+          Date.now() +
+          path.extname(file.originalname)
+      );
+    },
+  }),
+  limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
+}).single('imageFile');
+
+module.exports = profileImgUpload;
