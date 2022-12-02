afc2012579c2a5c748a4cf86ff3aaaf65bbc0d7e
Njoli Patrick <ogmaro@gmail.com>
2022-12-02 23:44:42 +0100

    chore: minor fix

--- packages/api/controller/auth/authThirdPartyController.js
+++ packages/api/controller/auth/authThirdPartyController.js
@@ -1,9 +1,19 @@
 const { OAuth2Client } = require("google-auth-library");
 const { response, authResponse } = require("../../utilities/response");
 const { environment } = require("../../config/environment");
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SERVER_ROOT_URI, LINKEDIN_URL_ENCODED, LINKEDIN_CALLBACK_URL, LINKEDIN_CLIENT_ID, LINKEDIN_SECRET_ID, FB_CLIENT_ID, FB_CLIENT_SERECT } = environment;
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  SERVER_ROOT_URI,
+  LINKEDIN_URL_ENCODED,
+  LINKEDIN_CALLBACK_URL,
+  LINKEDIN_CLIENT_ID,
+  LINKEDIN_SECRET_ID,
+  FB_CLIENT_ID,
+  FB_CLIENT_SERECT,
+} = environment;
 const Axios = require("axios");
-const querystring = require('query-string');
+const querystring = require("query-string");
 const { userCollection } = require("../../database/models/userSchema");
 const { register } = require("../../repository/user.repository");
 const { slugify } = require("../../utilities/compare");
@@ -12,7 +22,7 @@ const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
 async function googleAuthURL(req, res) {
   const options = {
-    redirect_uri: `${SERVER_ROOT_URI}/api/v1/auth/google`,
+    redirect_uri: `${SERVER_ROOT_URI}`,
     client_id: GOOGLE_CLIENT_ID,
     access_type: "offline",
     response_type: "code",
@@ -23,21 +33,21 @@ async function googleAuthURL(req, res) {
     ].join(" "),
   };
   const querystring = new URLSearchParams(options);
-  res.status(200).json(response({message: `${rootUrl}?${querystring}`}));
+  res.status(200).json(response({ message: `${rootUrl}?${querystring}` }));
 }
 async function getTokens(code) {
   try {
     /*
      * Uses the code to get tokens
      * that can be used to fetch the user's profile
-     */ 
+     */
     const client = new OAuth2Client(
       GOOGLE_CLIENT_ID,
       GOOGLE_CLIENT_SECRET,
-      `${SERVER_ROOT_URI}/api/v1/auth/google`
+      `${SERVER_ROOT_URI}`
     );
-    
-    const {res} = await client.getToken(code);
+
+    const { res } = await client.getToken(code);
 
     const data = {
       id_token: res.data.id_token,
@@ -54,127 +64,150 @@ async function getTokens(code) {
 
     return googleUser.data;
   } catch (error) {
-    return false
+    return false;
   }
 }
 
 const getLinkedinUrl = (req, res) => {
-	return res.status(200).json(response ({ message: "LinkedIn URL", success: true, data: `https://www.linkedin.com/oauth/v2/authorization?response_type=code&scope=r_liteprofile%20r_emailaddress&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${LINKEDIN_URL_ENCODED}` }));
-}
+  return res
+    .status(200)
+    .json(
+      response({
+        message: "LinkedIn URL",
+        success: true,
+        data: `https://www.linkedin.com/oauth/v2/authorization?response_type=code&scope=r_liteprofile%20r_emailaddress&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${LINKEDIN_URL_ENCODED}`,
+      })
+    );
+};
 
 /*
-	* Uses the code to get tokens
-	* that can be used to fetch the user's profile
-*/
-const getLinkedinAccessToken = async ({ code, clientId, clientSecret, redirectUri }) => {
-	 const url = "https://api.linkedin.com/oauth/v2/accessToken"
+ * Uses the code to get tokens
+ * that can be used to fetch the user's profile
+ */
+const getLinkedinAccessToken = async ({
+  code,
+  clientId,
+  clientSecret,
+  redirectUri,
+}) => {
+  const url = "https://api.linkedin.com/oauth/v2/accessToken";
 
-	 const values = {
-		code,
-		client_id: clientId,
-		client_secret: clientSecret,
-		redirect_uri: redirectUri,
-		grant_type: "authorization_code",
-	};
+  const values = {
+    code,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: "authorization_code",
+  };
 
-	const res_value = await Axios({
-		method: 'POST',
-		url,
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-		data: querystring.stringify(values)
-	}).catch((error) => {
-		console.log(`Failed to fetch auth tokens`);
-		console.error(error)
-	});
+  const res_value = await Axios({
+    method: "POST",
+    url,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: querystring.stringify(values),
+  }).catch((error) => {
+    console.log(`Failed to fetch auth tokens`);
+    console.error(error);
+  });
 
-	if (!res_value) {
-		return null
-	}
-	return res_value.data;
-}
+  if (!res_value) {
+    return null;
+  }
+  return res_value.data;
+};
 
-const linkedinAccessToken = async(req, res) => {
-	const { code } = req.query
-	try {
-		if (!code) {
-			return true
-		}
+const linkedinAccessToken = async (req, res) => {
+  const { code } = req.query;
+  try {
+    if (!code) {
+      return true;
+    }
 
-		const token = await getLinkedinAccessToken({
-			code,
-			redirectUri: LINKEDIN_CALLBACK_URL,
-			clientId: LINKEDIN_CLIENT_ID,
-			clientSecret: LINKEDIN_SECRET_ID
-		})
+    const token = await getLinkedinAccessToken({
+      code,
+      redirectUri: LINKEDIN_CALLBACK_URL,
+      clientId: LINKEDIN_CLIENT_ID,
+      clientSecret: LINKEDIN_SECRET_ID,
+    });
 
-		const linkedinUserEmail = await Axios({
-			method: "GET",
-			url:`https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))`,
-			headers: {
-				Authorization: `Bearer ${token.access_token}`,
-			},
-		}).catch((error) => {
-			console.error(`Failed to fetch details`);
-			console.log(error);
-		})
+    const linkedinUserEmail = await Axios({
+      method: "GET",
+      url: `https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))`,
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+      },
+    }).catch((error) => {
+      console.error(`Failed to fetch details`);
+      console.log(error);
+    });
 
-		const linkedinUser = await Axios({
-			method: "GET",
-			url:`https://api.linkedin.com/v2/me`,
-			headers: {
-				Authorization: `Bearer ${token.access_token}`,
-			},
-		}).catch((error) => {
-			console.log(`Failed to fetch details`);
-		})
+    const linkedinUser = await Axios({
+      method: "GET",
+      url: `https://api.linkedin.com/v2/me`,
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+      },
+    }).catch((error) => {
+      console.log(`Failed to fetch details`);
+    });
 
-		const email = linkedinUserEmail.data.elements[0]["handle~"].emailAddress
+    const email = linkedinUserEmail.data.elements[0]["handle~"].emailAddress;
 
-		//check if user exist then login
-		const existing_user = await userCollection.findOne({ email })
+    //check if user exist then login
+    const existing_user = await userCollection.findOne({ email });
 
-		if (existing_user) {
-			return res.status(200).json(response({ success: true,	message: "User login successfully",	data: authResponse(existing_user)	}))
-		}
+    if (existing_user) {
+      return res
+        .status(200)
+        .json(
+          response({
+            success: true,
+            message: "User login successfully",
+            data: authResponse(existing_user),
+          })
+        );
+    }
 
-		// create user
-		const fullName = `${linkedinUser.data.localizedFirstName} ${linkedinUser.data.localizedLastName}`
-		const randomUserCode = (Math.random() + 1).toString(36).substring(7);
+    // create user
+    const fullName = `${linkedinUser.data.localizedFirstName} ${linkedinUser.data.localizedLastName}`;
+    const randomUserCode = (Math.random() + 1).toString(36).substring(7);
 
-		const data = {
-			firstName: linkedinUser.data.localizedFirstName,
-			lastName: linkedinUser.data.localizedLastName,
-			email,
-			username: slugify(fullName) + randomUserCode,
-			password: null
-		};
-		const user = await register(data);
+    const data = {
+      firstName: linkedinUser.data.localizedFirstName,
+      lastName: linkedinUser.data.localizedLastName,
+      email,
+      username: slugify(fullName) + randomUserCode,
+      password: null,
+    };
+    const user = await register(data);
 
-		if (!user)
-			return res
-				.status(500)
-				.json(response({ success: false, message: "User not created" }));
+    if (!user)
+      return res
+        .status(500)
+        .json(response({ success: false, message: "User not created" }));
 
-		return res.status(201).json(
-			response({
-				success: true,
-				message: "User created successfully",
-				data: user,
-			})
-		)
-		
-	} catch (error) {
-		return res.status(500).json(
+    return res.status(201).json(
+      response({
+        success: true,
+        message: "User created successfully",
+        data: user,
+      })
+    );
+  } catch (error) {
+    return res.status(500).json(
       response({
         message: "Something went wrong wile processing this request",
         success: false,
       })
     );
-	}
-}
+  }
+};
 
-
-module.exports = { googleAuthURL, getTokens, getLinkedinUrl, linkedinAccessToken };
-
+module.exports = {
+  googleAuthURL,
+  getTokens,
+  getLinkedinUrl,
+  linkedinAccessToken,
+};
