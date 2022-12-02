32be6fd6ba453344b8a73d8dd158cdb79bc9865a
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2022-12-02 19:55:11 +0100

    feat: added navbar to api-status page

--- packages/web/src/pages/api-status/api-status.jsx
+++ packages/web/src/pages/api-status/api-status.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GrittyLogo from '../../assets/images/logo.webp';
+import Navbar from '../../components/Navbar';
 import Footer from '../../modules/static/landing-page/Footer';
 import style from './api-status.module.css';
 import Detail from './Report/ReportDetail';
@@ -7,32 +7,21 @@ import Detail from './Report/ReportDetail';
 const ApiStatus = () => {
   return (
     <>
-    <div className="statusPage">
-      <div className={style.header}>
-        <a href="/home" className={style.headerLink}>
-          <img
-            src={GrittyLogo}
-            alt="gritty grammar logo"
-            className={style.grittyImg}
-           // prevent image Jank
-          />
-        </a>
-      </div>
+      <Navbar />
+      <div className="statusPage">
+        <div>
+          <main className={style.mainContainer}>
+            <div className={style.mainHeader}>
+              <h1 className={style.title}>Speak better is Running </h1>
+              <p className={style.text}>API Status for gritty grammar is updating in realtime.</p>
+            </div>
 
-      {/* <MainStatus /> */}
-      <div>
-        <main className={style.mainContainer}>
-          <div className={style.mainHeader}>
-            <h1 className={style.title}>Speak better is Running </h1>
-            <p className={style.text}>API Status for gritty grammar is updating in realtime.</p>
-          </div>
-
-          <Detail />
-        </main>
+            <Detail />
+          </main>
+        </div>
       </div>
-    </div>
       <Footer />
-      </>
+    </>
   );
 };
 
