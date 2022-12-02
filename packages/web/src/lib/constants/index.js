d4e0255d35661f29d9b5393ca1ed9a4d529f4be6
Emmanuel Ayo <94123198+bohemiancode-x@users.noreply.github.com>
2022-12-02 23:25:30 +0100

    Revert "chore- update base Api URL"

--- packages/web/src/lib/constants/index.js
+++ packages/web/src/lib/constants/index.js
@@ -1,5 +1,5 @@
 export const ENDPOINTS = {
-  API_BASE_URL: 'https://api.speakbetter.hng.tech/v1/',
+  API_BASE_URL: 'http://api.speakbetter.hng.tech/v1/',
   API_AUTH_LOGIN: 'auth/login',
   API_INITIATE_SIGNUP: 'auth/signup',
   API_SEND_AUDIO: 'conversation/sendAudio',
