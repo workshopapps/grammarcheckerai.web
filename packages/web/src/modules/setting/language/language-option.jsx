acd4a2933aa289019b73d4f55885a1c6d8efd0d3
Ajayi Michael <ajayimichael150@gmail.com>
2022-12-03 17:40:16 +0100

    feat: add search field functionality in settings language

--- packages/web/src/modules/setting/language/language-option.jsx
+++ packages/web/src/modules/setting/language/language-option.jsx
@@ -1,7 +1,10 @@
 import { arrowRightIcon, checkIcon, searchIcon } from '../../../../assets';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function LanguageOption({ openBar, changeLanguage, languageList }) {
+  const [searchTerm, setsearchTerm] = useState('');
+
   return (
     <div className="absolute top-0 left-0 z-[150] w-full h-screen bg-dark-100 px-6 py-5 overflow-y-auto">
       <div className="max-w-screen-xl mx-auto">
@@ -16,35 +19,47 @@ function LanguageOption({ openBar, changeLanguage, languageList }) {
             type="text"
             placeholder="Find a Language"
             className="w-full border bg-transparent border-gray-300 rounded-md px-3 py-2 font-normal"
+            onChange={(event) => {
+              setsearchTerm(event.target.value);
+            }}
           />
           <img className="absolute right-3 block" src={searchIcon} alt="Find a language" />
         </div>
         <div className="flex flex-col gap-8 h-6/12">
-          {languageList.map((language, index) => {
-            return (
-              <div key={index} className="relative flex justify-between items-center">
-                <button
-                  className="absolute w-full h-full"
-                  onClick={() => {
-                    changeLanguage(language);
-                  }}
-                ></button>
-                <div className="w-full flex items-center gap-3 font-normal">
-                  <img src={language.flag} alt={language.name} />
-                  <p>{language.name}</p>
+          {languageList
+            .filter((obj) => {
+              if (searchTerm === '' || obj.query.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return obj;
+              } else if (settingList.every((val) => !val.query.toLowerCase().includes(searchTerm.toLowerCase()))) {
+                // If There is no result then show all list
+                return obj;
+              }
+            })
+            .map((language, index) => {
+              return (
+                <div key={index} className="relative flex justify-between items-center">
+                  <button
+                    className="absolute w-full h-full"
+                    onClick={() => {
+                      changeLanguage(language);
+                    }}
+                  ></button>
+                  <div className="w-full flex items-center gap-3 font-normal">
+                    <img src={language.flag} alt={language.name} />
+                    <p>{language.name}</p>
+                  </div>
+                  <div
+                    className={
+                      language.selected
+                        ? 'w-6 h-6 rounded-full bg-purple-500 border-2 flex justify-center items-center'
+                        : 'w-6 h-6 rounded-full bg-white border-2 border-gray-300'
+                    }
+                  >
+                    {language.selected && <img src={checkIcon} alt="check" />}
+                  </div>
                 </div>
-                <div
-                  className={
-                    language.selected
-                      ? 'w-6 h-6 rounded-full bg-purple-500 border-2 flex justify-center items-center'
-                      : 'w-6 h-6 rounded-full bg-white border-2 border-gray-300'
-                  }
-                >
-                  {language.selected && <img src={checkIcon} alt="check" />}
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     </div>
