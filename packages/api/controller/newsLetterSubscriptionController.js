013f0fff86f139f030c8a05d71b2746f1fdda550
Olúbùsọ́lá Odunuga <olubusolao3@gmail.com>
2022-12-03 23:05:29 +0100

    feat/BAC2-25: Create a newsletter Endpoint using cronjob, modify the code-base and added a 404 error message endpoint

---
+++ packages/api/controller/newsLetterSubscriptionController.js
@@ -0,0 +1,36 @@
+const { userCollection } = require("../database/models/userSchema");
+const newsLetterSubscription = require("../database/models/newsLetterSubscriptionSchema");
+const newsLetterService = require("../services/newsletter")
+
+//query user collection
+exports.isSubscribe = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const subscribed_user = await newsLetterSubscription.findOne({ email });
+    if (subscribed_user) {
+      res.status(200).json({
+        status: true,
+        message: "You are already a subscriber, chill for our newsletters",
+      });
+    }
+    const user_exist = await userCollection.findOne({ email });
+    let user_id;
+    if (user_exist) {
+      user_id = user_exist._id;
+    }
+    await newsLetterSubscription.create({
+      email,
+      user_id: user_id || null,
+    });
+    await newsLetterService.send(email)
+    res.status(200).json({
+      status: true,
+      message: "You are now subscribed to our newsletter",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "something went wrong while handling your request",
+    });
+  }
+};
