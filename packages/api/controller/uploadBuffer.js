db24e344355cb5173450ad652993138abd90c5ef
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-12-03 23:18:39 +0100

    fix: uuid import

--- packages/api/controller/uploadBuffer.js
+++ packages/api/controller/uploadBuffer.js
@@ -1,48 +1,44 @@
-const AWS = require('aws-sdk');
-const { environment } = require('../config/environment')
-const v4 = require("uuid")
+const AWS = require("aws-sdk");
+const { environment } = require("../config/environment");
+const { v4 } = require("uuid");
 const { ACCESSKEYID, S3SECRETEKEY, GRITTYBUCKETNAME } = environment;
 
 // Set the AWS Region.
 const REGION = "eu-west-1";
 
 const s3 = new AWS.S3({
-    region: REGION,
-    credentials: {
-        accessKeyId: ACCESSKEYID,
-        secretAccessKey: S3SECRETEKEY
-    }
+  region: REGION,
+  credentials: {
+    accessKeyId: ACCESSKEYID,
+    secretAccessKey: S3SECRETEKEY,
+  },
 });
 
 const fileUploadToS3Bucket = async (dataBuffer) => {
-    try {
-        // Setting up S3 upload parameters
-        const params = {
-            Bucket: GRITTYBUCKETNAME,
-            Key: v4(), // File name you want to save as in S3 // Change the key value to UUID or GUID
-            Body: dataBuffer,
-        };
-        
-        // Uploading files to the bucket
-        let promise = new Promise((resolve, reject) => {
-            s3.upload(params, function (err, data) {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve(data.Location);
-                }
-            })
-        })
-
-        let fileLocation = await promise;
-        return fileLocation;
-    }
-
-    catch (err) {
-        console.error(`Error: ${err.message}`);
+  try {
+    // Setting up S3 upload parameters
+    const params = {
+      Bucket: GRITTYBUCKETNAME,
+      Key: `${v4()}.mp3`, // File name you want to save as in S3
+      Body: dataBuffer,
     };
+
+    // Uploading files to the bucket
+    let promise = new Promise((resolve, reject) => {
+      s3.upload(params, function (err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data.Location);
+        }
+      });
+    });
+
+    let fileLocation = await promise;
+    return fileLocation;
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
 };
 
-
-module.exports = fileUploadToS3Bucket
+module.exports = fileUploadToS3Bucket;
