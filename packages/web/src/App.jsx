51fff106c98012dc34411a52afbd80715bdbcb25
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-12-03 23:03:17 +0200

    chore: make payment and subscribe user

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -47,7 +47,9 @@ const Ai = lazy(() => import('./pages/Blog/Ai'));
 const Grammar = lazy(() => import('./pages/Blog/Grammar'));
 const Tips = lazy(() => import('./pages/Blog/Tips'));
 const Contact = lazy(() => import('./pages/contact/index'));
-const PremiumSubs = lazy(() => import('./modules/premium/index'));
+const PremiumSubs = lazy(() => import('./modules/premium/popup/premium'));
+const SubscriptionHistory = lazy(() => import('./modules/premium/index'));
+
 // All routes/pages must be import from ./pages folder
 
 const DashboardLayout = () => (
@@ -64,6 +66,11 @@ const Signuptwo = () => (
 const Premium = () => (
   <Suspense fallback={<Fallback />}>
     <PremiumSubs />
+  </Suspense>
+);
+const Subscription = () => (
+  <Suspense fallback={<Fallback />}>
+    <SubscriptionHistory />
   </Suspense>
 );
 
@@ -352,6 +359,7 @@ function App() {
         <Route path="profile/deleteaccount-step2" element={<ConfirmDeleteAccount />} />
         <Route path="import" element={<TranscribePage />} />
         <Route path="settings" element={<Settings />} />
+        <Route path="subscription" element={<Subscription />} />
       </Route>
     </Routes>
   );
--- packages/web/src/modules/premium/index.jsx
+++ packages/web/src/modules/premium/index.jsx
@@ -1,93 +1,10 @@
-import React, { useState } from 'react';
-import { PaystackButton } from 'react-paystack';
-import { TextField, Button } from '@mui/material';
-import usePremium from '../../hooks/auth/usePremium';
-const Premium = () => {
-  const planPremium = usePremium();
-  const publicKey = 'pk_test_0c3ed4c2ad95f30b182a119b48ae9f99e9c4cb5c';
-  const amount = 100;
-  const currency = 'ZAR';
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+import React from 'react';
+import PropTypes from 'prop-types';
 
-  const componentProps = {
-    reference: new Date().getTime().toString(),
-    email,
-    amount,
-    currency,
-    metadata: {
-      name,
-    },
-    publicKey,
-    text: 'Subscribe',
-
-    onSuccess: () => {
-      planPremium
-        .mutateAsync({
-          customer: email,
-          plan: 'PLN_4yhibhhlmcw51j4',
-        })
-        .then((res) => {
-          console.log(res);
-          setName(res);
-        })
-        .then((result) => {})
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    onClose: () => alert('njddsdhj'),
-  };
-
-  const useFetch = (url) => {
-    var requestOptions = {
-      method: 'GET',
-      customer: email,
-      headers: {
-        Authorization: 'Bearer sk_test_7c8d4865357e9ee081faf517549b011044a8cd12',
-      },
-    };
-
-    fetch(url, requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
-        const oBJ = JSON.parse(result);
-        console.log(oBJ);
-      })
-      .catch((err) => console.log(err.message));
-  };
-
-  const handleUserSubs = () => {
-    useFetch('https://api.paystack.co/subscription');
-  };
-
-  return (
-    <div>
-      <div>
-        <TextField
-          fullWidth
-          label="Full names"
-          id="fullWidth"
-          value={name}
-          type={'text'}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          fullWidth
-          label="email"
-          id="fullWidth"
-          type={'email'}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        {email !== '' && name !== '' ? <PaystackButton {...componentProps} /> : null}
-      </div>
-      <Button onClick={handleUserSubs} variant="outlined" color="primary">
-        Get user List
-      </Button>
-    </div>
-  );
+const Subscription = (props) => {
+  return <div>Subscription</div>;
 };
 
-export default Premium;
+Subscription.propTypes = {};
+
+export default Subscription;
