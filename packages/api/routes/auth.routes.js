b49f706c6a1c58e5b5b8e9904512c97d5c8f2ac1
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-12-04 15:14:46 +0100

    Facebook auth and facebook redirect link fix

--- packages/api/routes/auth.routes.js
+++ packages/api/routes/auth.routes.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const auth = Router();
 const {
   validate,
@@ -6,46 +6,48 @@ const {
   reset_password,
   request_reset_password,
   loginValidationRules,
-} = require("../utilities/validation/auth.validation");
+} = require('../utilities/validation/auth.validation');
 const {
   registerUser,
   verifyMail,
   googleAuthUserSignUp,
   login,
-} = require("../controller/auth/user.controller");
+} = require('../controller/auth/user.controller');
 const {
   googleAuthURL,
   getLinkedinUrl,
   linkedinAccessToken,
-	getFacebookURl,
-	facebookAccessToken
-} = require("../controller/auth/authThirdPartyController"); 
-const { logout } = require("../controller/logoutcontroller");
+  getFacebookURl,
+  facebookAccessToken,
+} = require('../controller/auth/authThirdPartyController');
+const { logout } = require('../controller/logoutcontroller');
 
 const {
   requestForgotPassword,
   resetPassword,
-} = require("../controller/forgotPasswordController");
+} = require('../controller/forgotPasswordController');
+const { facebook } = require('./facebookAuth');
 
-auth.post("/signup", registerValidationRules(), validate, registerUser);
-auth.get("/verify/:link", verifyMail);
+auth.post('/signup', registerValidationRules(), validate, registerUser);
+auth.get('/verify/:link', verifyMail);
 
-auth.get("/google", googleAuthURL);
-auth.post("/google", googleAuthUserSignUp);
+auth.get('/google', googleAuthURL);
+auth.post('/google', googleAuthUserSignUp);
 
 auth.post(
-  "/request-password-reset",
+  '/request-password-reset',
   request_reset_password(),
   validate,
   requestForgotPassword
 );
-auth.post("/password-reset", reset_password(), validate, resetPassword);
+auth.post('/password-reset', reset_password(), validate, resetPassword);
 
-auth.post("/login", loginValidationRules(), validate, login);
-auth.post("/logout", logout);
-auth.get("/linkedin", getLinkedinUrl);
-auth.get("/linkedin/callback", linkedinAccessToken);
-auth.get("/facebook", getFacebookURl);
-auth.get("/facebook/callback", facebookAccessToken);
+auth.post('/login', loginValidationRules(), validate, login);
+auth.post('/logout', logout);
+auth.get('/linkedin', getLinkedinUrl);
+auth.get('/linkedin/callback', linkedinAccessToken);
+// auth.get("/facebook", getFacebookURl);
+// auth.get("/facebook/callback", facebookAccessToken);
+auth.use('/facebook', facebook);
 
 module.exports = { auth };
