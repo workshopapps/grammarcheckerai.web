00e0379db7a7cc3ebc16b7a240f6d6fe40465c60
Emmanuel Ayo <emmieaddiz@gmail.com>
2022-12-05 20:25:07 +0100

    chore: minor fix

--- packages/web/src/modules/account/home/homePage.jsx
+++ packages/web/src/modules/account/home/homePage.jsx
@@ -8,10 +8,10 @@ function HomePage() {
     <div className="flex flex-col pt-16 lg:ml-[62px] lg:mr-[9rem] sm:mx-[70px] mx-8">
       <div className="heading flex justify-between items-center font-['DM_Sans']">
         <h3 className=" text-[#393939] sm:text-[32px] font-normal text-[24px] leading-10">
-          Hello <b>{userData.username},</b>
+          Hello <b>{userData ? userData.username : ''},</b>
         </h3>
         <span className="w-[26px] h-[23px]  rounded-[50%] bg-[#8C54BF] p-[22px] text-white text-[18px] flex justify-center items-center font-medium">
-        {userData.firstName.charAt(0) + '' + userData.lastName.charAt(0)}
+        {userData ? userData.firstName.charAt(0) + '' + userData.lastName.charAt(0) : ""}
         </span>
       </div>
       <div className="flex justify-center items-center flex-col mt-8 font-['inter']">
