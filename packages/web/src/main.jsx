c60f9ec159664a4c6f24fe595e6894243835d94b
Julius Omoleye <joshkid610@gmail.com>
2022-12-06 15:40:25 +0100

    report web vitals

--- packages/web/src/main.jsx
+++ packages/web/src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AuthProvider from './lib/context/AuthContext';
 import '../src/assets/styles/styles.scss';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 import DarkThemeContext from './lib/context/DarkThemeContext';
@@ -38,3 +39,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </DarkThemeContext>
   </React.StrictMode>,
 );
+
+reportWebVitals();
