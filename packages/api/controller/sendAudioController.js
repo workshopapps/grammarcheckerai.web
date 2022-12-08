a08a91a7a4e101ecc4ec8358eb9a9e5af713c4f3
Njoli Patrick <ogmaro@gmail.com>
2022-12-08 13:56:57 +0100

    added console log

--- packages/api/controller/sendAudioController.js
+++ packages/api/controller/sendAudioController.js
@@ -34,7 +34,8 @@ async function getBotResponse(req, res) {
     const userId = req.body.userId;
     const language = req.body.language?.toLowerCase() || "english";
     const audioFile = req.file; // retrieves file buffer and metadata set by multer
-
+    
+    console.log(audioFile)
     // checks if file is available
     if (!audioFile) {
       return res.status(400).send({
