b27dcbd0e0bff2530ac63f4b9441fe695b6ddbf8
Njoli Patrick <ogmaro@gmail.com>
2022-12-08 11:20:07 +0100

    added devopes file

--- packages/api/server.js
+++ packages/api/server.js
@@ -1,9 +1,19 @@
+// It must be placed above all other 'require' statements
+var atatus = require("atatus-nodejs");
+atatus.start({
+    licenseKey: "lic_apm_2c415fffd3d94ee68e8daf25b02ae5ee",
+    appName: "Speakbetter",
+});
+
 require('newrelic');
+
+
 
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 // import * as Sentry from "@sentry/node";
 // import * as Tracing from "@sentry/tracing";
+
 
 const app = require("./app");
 const { environment } = require("./config/environment.js");
