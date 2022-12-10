9e84c37aab053fc997baee26d8960a416dadef6d
Oghenesuvwe Money <rhounamoney@gmail.com>
2022-12-10 20:37:47 +0100

    feat: new design implemented

--- packages/web/src/modules/static/quizgame/QuizContext.jsx
+++ packages/web/src/modules/static/quizgame/QuizContext.jsx
@@ -5,16 +5,20 @@ import { io } from 'socket.io-client';
 export const QuizContext = createContext();
 
 const QuizContextProvider = (props) => {
-  const userId = localStorage.getItem('grittyuserid');
+  // const userId = localStorage.getItem('grittyuserid');
   const socket = io('https://api.speakbetter.hng.tech/', { autoConnect: false });
 
-  if (userId && (userId !== null || userId !== '')) {
+  // if (userId 
+  //   // && 
+  //   // (userId !== null || userId !== '')
+  //   ) {
     socket.on('connect', () => {
       console.log(`You connected with ${socket.id}`);
     });
-  } else {
-    console.log('You must be a logged in user');
-  }
+  // }
+  //  else {
+  //   console.log('You must be a logged in user');
+  // }
 
   return <QuizContext.Provider value={{ socket }}>{props.children}</QuizContext.Provider>;
 };
