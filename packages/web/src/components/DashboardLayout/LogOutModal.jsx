bdfa788ffca9f4eaa3281e4bc6e8d5cd5b172186
johnbosco <johnboscoo@phreetech.com>
2022-12-10 11:51:28 +0100

    chore: fixed logout modal issues

--- packages/web/src/components/DashboardLayout/LogOutModal.jsx
+++ packages/web/src/components/DashboardLayout/LogOutModal.jsx
@@ -1,7 +1,8 @@
 import ProfileScreenButton from '../Button/profileButton/ProfileScreenButton';
 import PropTypes from 'prop-types';
+import Modal from '@mui/material/Modal';
 
-export default function LogOutModal({modal, toggleModal}) {
+export default function LogOutModal({ handleClose, open }) {
   const onSignOut = () => {
     localStorage.clear();
     window.location.replace('/signin');
@@ -9,19 +10,29 @@ export default function LogOutModal({modal, toggleModal}) {
   };
 
   return (
-    <div className={modal ? 'absolute top-0 left-0 bg-black/70 min-h-screen max-h-full w-full flex justify-center z-[999]' : 'hidden'}>
-        <div className='w-[70%] md:w-[50%] flex flex-col items-center bg-white rounded m-auto p-5'>
-            <h3 className='text-xl'>Are you sure you want to log out?</h3>
-            <div className='w-full flex flex-col sm:flex-row lg:flex-row justify-center gap-5 mt-10'>
-                <ProfileScreenButton onClick={onSignOut}>Yes, log me out.</ProfileScreenButton>
-                <ProfileScreenButton onClick={() => toggleModal()} variant="danger">Cancel</ProfileScreenButton>
-            </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div
+        className="w-[90%] sm:w-[50%] flex flex-col items-center bg-white my-auto rounded p-5 py-8 absolute"
+        style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+      >
+        <h3 className="text-xl">Are you sure you want to log out?</h3>
+        <div className="w-full flex flex-col sm:flex-row lg:flex-row justify-center gap-5 mt-10">
+          <ProfileScreenButton onClick={onSignOut}>Yes, log me out.</ProfileScreenButton>
+          <ProfileScreenButton onClick={handleClose} variant="danger">
+            Cancel
+          </ProfileScreenButton>
         </div>
-    </div>
-  )
-};
+      </div>
+    </Modal>
+  );
+}
 
 LogOutModal.propTypes = {
-  modal: PropTypes.bool,
-  toggleModal: PropTypes.func,
+  handleClose: PropTypes.func,
+  open: PropTypes.bool,
 };
--- packages/web/src/components/DashboardLayout/index.jsx
+++ packages/web/src/components/DashboardLayout/index.jsx
@@ -9,11 +9,9 @@ import { AiFillWechat } from 'react-icons/ai';
 import LogOutModal from './LogOutModal';
 
 function DashboardLayout() {
-  const [modal, setModal] = useState(false);
-
-  const toggleModal = () => {
-    setModal(!modal);
-  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div className="flex flex-col-reverse sm:flex-row  min-h-screen">
@@ -46,7 +44,7 @@ function DashboardLayout() {
             Quiz
           </SidebarLink>
           <button
-            onClick={() => toggleModal()}
+            onClick={() => handleOpen()}
             className="mt-10 flex w-full  pl-16 border-r-4 py-5 border-[#5D387F00] hover:border-[#5D387F33] items-center gap-5 m-auto"
           >
             <BsArrowBarRight className="" />
@@ -57,7 +55,7 @@ function DashboardLayout() {
       <div className="h-[10%]">
         <MobileMenu />
       </div>
-      <LogOutModal modal={modal} toggleModal={toggleModal} />
+      <LogOutModal handleClose={handleClose} open={open} />
       <div className="w-full h-[90%] sm:pt-5">
         {/* <div className="py-4 border-b border-slate-300 w-full"></div> */}
         <Outlet />
