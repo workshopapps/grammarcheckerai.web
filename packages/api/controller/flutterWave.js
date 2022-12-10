aff0e94b20b472eed3b625365424b81e49e5047e
Njoli Patrick <ogmaro@gmail.com>
2022-12-10 12:06:57 +0100

    chore: gotten data from flutterwave

--- packages/api/controller/flutterWave.js
+++ packages/api/controller/flutterWave.js
@@ -1,68 +1,72 @@
-const Flutterwave = require('flutterwave-node-v3');
+const Flutterwave = require("flutterwave-node-v3");
 require("dotenv").config();
-const axios = require("axios")
-const got = require("got");
-const { response } = require('express');
+// const axios = require("axios")
+// const got = require("got");
+const { response } = require("express");
 
-
-
-
-const flw = new Flutterwave('FLWPUBK_TEST-ab96bfc24d101b7af5933ca120fdb1a7-X', 'FLWSECK_TEST-204488a850fd0dca39e00cfba88d40f6-X'  );
+const flw = new Flutterwave(
+  "FLWPUBK_TEST-ab96bfc24d101b7af5933ca120fdb1a7-X",
+  "FLWSECK_TEST-204488a850fd0dca39e00cfba88d40f6-X"
+);
 
 const createPaymentPlan = async () => {
+  try {
+    const payload = {
+      amount: 500,
+      name: "kelechi", //This is the name of the payment, it will appear on the subscription reminder emails
+      interval: "monthly", //This will determine the frequency of the charges for this plan. Could be monthly, weekly, etc.
+      duration: 24, //This is the frequency, it is numeric, e.g. if set to 5 and intervals is set to monthly you would be charged 5 months, and then the subscription stops
+    };
 
-    try {
-
-        const payload = {
-            "amount": 500,
-            "name": "kelechi", //This is the name of the payment, it will appear on the subscription reminder emails
-            "interval": "monthly", //This will determine the frequency of the charges for this plan. Could be monthly, weekly, etc.
-            "duration": 24 //This is the frequency, it is numeric, e.g. if set to 5 and intervals is set to monthly you would be charged 5 months, and then the subscription stops
-            
-        }
-
-        const response = await flw.PaymentPlan.create(payload)
-        console.log(response);
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
+    const response = await flw.PaymentPlan.create(payload);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // createPaymentPlan();
 
 const fetchSubscription = async () => {
-
-    try {
-        
-        const response = await flw.Subscription.fetch_all()
-        console.log(response);
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
+  try {
+    const response = await flw.Subscription.fetch_all();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // fetchSubscription();
 
-
 const activateSubscription = async () => {
-
-    try {
-        const payload={
-            "id":"31036" //This is the unique id of the subscription you want to activate. It is returned in the Get a subscription call as data.id
-        }
-        
-        const response = await flw.Subscription.activate(payload)
-        console.log(response);
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
+  try {
+    const payload = {
+      tx_ref: "MC-3243f",
+      amount: 4,
+      currency: "USD",
+      redirect_url: "https://speakbetter.hng.tech",
+      customer: { email: "ogmaro@yopmail.com" },
+      payment_plan: 31036,
+      email: "ogmaro@yopmail.com",
+      card_number: "4556052704172643",
+      enckey: "FLWSECK_TESTb780896f15ad",
+      expiry_month: "01",
+      expiry_year: "23",
+      authorization: {
+        mode: 'avs_noauth',
+        city: "AMAC",
+        address: "My Addess",
+        state: "Abuja",
+        country: "Nigeria",
+        zipcode: 30001,
+      },
+    };
+    const response = await flw.Charge.card(payload);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 activateSubscription();
 
@@ -100,51 +104,46 @@ activateSubscription();
 //         console.log(err.code);
 //         console.log(err.response);
 //     }
-        
+
 // }
 
 //  pay()
 
+// const headers = {
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${token}`
+// }
 
+// const json =  {
+//     tx_ref: "hooli-tx-1920bbtytty",
+//     amount: "100",
+//     currency: "NGN",
+//     redirect_url: "https://webhook.site/9d0b00ba-9a69-44fa-a43d-a82c33c36fdc",
+//     meta: {
+//         consumer_id: 23,
+//         consumer_mac: "92a3-912ba-1192a"
+//     },
+//     customer: {
+//         email: "khayceesdike@gmail.com",
+//         phonenumber: "080****4528",
+//         name: "Yemi Desola"
+//     },
+//     customizations: {
+//         title: "Pied Piper Payments",
+//         logo: "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
+//     }
+// }
 
- // const headers = {
-    //     'Content-Type': 'application/json',
-    //     Authorization: `Bearer ${token}`
-    // }
-
-    // const json =  {
-    //     tx_ref: "hooli-tx-1920bbtytty",
-    //     amount: "100",
-    //     currency: "NGN",
-    //     redirect_url: "https://webhook.site/9d0b00ba-9a69-44fa-a43d-a82c33c36fdc",
-    //     meta: {
-    //         consumer_id: 23,
-    //         consumer_mac: "92a3-912ba-1192a"
-    //     },
-    //     customer: {
-    //         email: "khayceesdike@gmail.com",
-    //         phonenumber: "080****4528",
-    //         name: "Yemi Desola"
-    //     },
-    //     customizations: {
-    //         title: "Pied Piper Payments",
-    //         logo: "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
-    //     }
-    // }
-
-    //     const response = await got.post("https://api.flutterwave.com/v3/payments", {
-    //         headers: headers,
-    //         data: json
-    //     }).then((data)=>{
-    //       console.log(data);
-    //     })
-    //     .catch((error)=>{
-    //       console.log(error);
-    //     })
-    // } 
-
-
-
+//     const response = await got.post("https://api.flutterwave.com/v3/payments", {
+//         headers: headers,
+//         data: json
+//     }).then((data)=>{
+//       console.log(data);
+//     })
+//     .catch((error)=>{
+//       console.log(error);
+//     })
+// }
 
 // const flw = new Flutterwave("FLWPUBK_TEST-7e3146a9c1bf7af86f1e7a5cfeadefed-X", "FLWSECK_TEST-d9bca844bd11eba144a5430ba30d687d-X");
 // const details = {
