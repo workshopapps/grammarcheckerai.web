d0c1b9981aa5b9a7a04718bcb374eacac8b0664a
Emmanuel Ayo <emmieaddiz@gmail.com>
2022-12-13 18:53:29 +0100

    chore: login error message and other minor fixes

--- packages/web/src/pages/Legal/Trademark.jsx
+++ packages/web/src/pages/Legal/Trademark.jsx
@@ -9,10 +9,10 @@ export default function Trademark() {
   };
   return (
     <div>
-      <div className='py-14 px-4 text-center bg-[#E8DDF2] text-BLACK'>
-      <div className="goback pb-30">
-                    <button onClick={handlePrev} className="bg-purple-500 hover:bg-purple-600 text-white pl-2 ml-2 flex items-center mt-0 lg:absolute w-36 h-12 sm: relative mb-8 rounded xl:mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='h-8' >
+      <div className='py-14 px-4 text-center relative bg-[#E8DDF2] text-BLACK'>
+      <div className="goback absolute top-2">
+                    <button onClick={handlePrev} className="text-xs sm:text-base text-[#5d387f] p-2 flex items-center rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='h-4 sm:h-5' >
                             <path
                                 fill="none"
                                 stroke="currentColor"
@@ -22,9 +22,10 @@ export default function Trademark() {
                                 d="M328 112L184 256l144 144"
                             />
                         </svg>
-            <span>Go back</span>
-          </button>
-        </div>
+                        <span>Go back</span>
+                    </button>
+
+                </div>
         <h1 className='md:text-3xl text-2xl font-bold'>Trademark and Copyright Policy</h1>
         <p>Last updated: 12 November, 2022.</p>
       </div>
